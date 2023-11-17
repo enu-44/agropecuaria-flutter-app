fb17829bc0a7df0c51ddf403732c32fee2c9166d
enu-44 <enuarmunozcastillo@gmail.com>
2023-11-17 17:36:19 -0500

    se agrega Hive como moor de BD

--- lib/features/data/datasources/hive/config/datasource_hive_instance.dart
+++ lib/features/data/datasources/hive/config/datasource_hive_instance.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:agropecuariosapp/features/data/datasources/hive/constant/hive_box_const.dart';
 import 'package:agropecuariosapp/features/data/datasources/hive/constant/hive_typeid_const.dart';
 import 'package:agropecuariosapp/features/data/datasources/hive/entities/account_hive.dart';
 import 'package:agropecuariosapp/features/data/datasources/hive/entities/animal_hive.dart';
@@ -20,6 +21,17 @@ mixin DataSourceHiveInstance {
     }
     if (!Hive.isAdapterRegistered(HiveTypeIdConst.TypeAnimalTypeHive)) {
       Hive.registerAdapter(AnimalTypeHiveAdapter());
+    }
+
+    //Open Box - Here
+    if (!Hive.isBoxOpen(HiveBoxConst.kAccountBoxName)) {
+      await Hive.openBox<AccountEntityHive>(HiveBoxConst.kAccountBoxName);
+    }
+    if (!Hive.isBoxOpen(HiveBoxConst.kAnimalBoxName)) {
+      await Hive.openBox<AnimalEntityHive>(HiveBoxConst.kAnimalBoxName);
+    }
+    if (!Hive.isBoxOpen(HiveBoxConst.kAnimalTypeBoxName)) {
+      await Hive.openBox<AnimalTypeEntityHive>(HiveBoxConst.kAnimalTypeBoxName);
     }
     return Hive;
   }
